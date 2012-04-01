f913e5dad7bb7822b658fd7b6aedf5a627e9f190
David Beveridge <davidjbeveridge@gmail.com>
2012-03-31 21:52:10 -0700

    Require sprockets/engines

--- lib/spine-rails-icanhaz.rb
+++ lib/spine-rails-icanhaz.rb
@@ -10,7 +10,8 @@ require 'js_renderer'
 require 'tilt-icanhaz_template'
 
 module SpineRailsIcanhaz
-
+  
+  require 'sprockets/engines' unless Sprockets.respond_to?(:register_engine)
   Sprockets.register_engine '.ich', Tilt::IcanhazTemplate
 
 end
